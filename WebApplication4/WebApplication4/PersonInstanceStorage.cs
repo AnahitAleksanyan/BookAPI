289c5eceb685bd9e74055bd4c64a3097756a2cbc
Lusine-Karapetyan <karapetyanlus.1991@gmail.com>
2023-03-11 00:19:29 +0400

    Create person GetAll endpoint
    Create person GetById endpoint
    Create person Create endpoint
    Create person delete Create

---
+++ WebApplication4/WebApplication4/PersonInstanceStorage.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication4.Repositories;
+
+namespace WebApplication4
+{
+    public class PersonInstanceStorage
+    {
+        public static readonly PersonRepository personRepository = new PersonRepository();
+
+    }
+}
