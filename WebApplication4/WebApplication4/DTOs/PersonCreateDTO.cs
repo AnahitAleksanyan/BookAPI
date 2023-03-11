c7805d5063762f04d8734c710d4c4a798a61b359
Anahit Aleksanyan <anahit_aleksanyan@epam.com>
2023-03-11 11:28:41 +0400

    created DTOs

---
+++ WebApplication4/WebApplication4/DTOs/PersonCreateDTO.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication4.Models;
+
+namespace WebApplication4.DTOs
+{
+    public class PersonCreateDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person ToPerson()
+        {
+            Person person = new Person()
+            {
+                Age = this.Age,
+                Name = this.Name,
+                Surname = this.Surname,
+            };
+
+            return person;
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/DTOs/PersonUpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication4.Models;
+
+namespace WebApplication4.DTOs
+{
+    public class PersonUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person ToPerson()
+        {
+            Person person = new Person()
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Surname = this.Surname,
+            };
+
+            return person;
+        }
+    }
+}
